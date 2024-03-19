424f2dd943ba8076044d64250368e8f5938dd420
Arrys Hanry Nobrega Wanderley <CapnArrys@academicos.com>
2024-03-19 20:50:10 -0300

    finalizando genero

--- src/main/java/br/com/fujideia/iesp/tecback/controller/GeneroController.java
+++ src/main/java/br/com/fujideia/iesp/tecback/controller/GeneroController.java
@@ -1,4 +1,28 @@
 package br.com.fujideia.iesp.tecback.controller;
 
+import br.com.fujideia.iesp.tecback.model.Filme;
+import br.com.fujideia.iesp.tecback.model.Genero;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.fujideia.iesp.tecback.service.GeneroService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/genero")
+
 public class GeneroController {
+    @Autowired
+    private GeneroService generoservice;
+
+    @PostMapping
+    public Genero salvar(@RequestBody Genero genero){
+        return generoservice.salvar(genero);
+    }
+
+
+
 }
