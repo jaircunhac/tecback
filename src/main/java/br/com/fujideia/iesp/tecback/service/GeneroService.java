12ba5cdd93381807297458e3895585a49b576073
jaircunhac <jaircunhacneto@gmail.com>
2024-03-19 20:47:44 -0300

    Correction of the GenreService

--- src/main/java/br/com/fujideia/iesp/tecback/service/GeneroService.java
+++ src/main/java/br/com/fujideia/iesp/tecback/service/GeneroService.java
@@ -3,15 +3,16 @@ package br.com.fujideia.iesp.tecback.service;
 import br.com.fujideia.iesp.tecback.model.Filme;
 import br.com.fujideia.iesp.tecback.model.Genero;
 import br.com.fujideia.iesp.tecback.repository.GeneroRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@AllArgsConstructor
+@Service
 public class GeneroService {
-        @Service
-        @Autowired
-        private GeneroRepository generoRepository;
+    @Autowired
+    private GeneroRepository generoRepository;
 
     public Genero salvar(Genero genero){
         return generoRepository.save(genero);
@@ -28,13 +29,11 @@ public class GeneroService {
         return generoRepository.save(genero);
     }
 
-    public Genero buscarPorId(Integer id){
+    public Genero buscarPorId(Long id){
         return generoRepository.findById(id).get();
     }
 
-    public void excluir(Integer id){
+    public void excluir(Long id){
         generoRepository.deleteById(id);
     }
-    }
-
 }
